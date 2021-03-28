2b296846de8a0456c6030a4afce56b1316f557dd
davidnyberg <dnyberg95@gmail.com>
2021-03-28 22:53:24 +0200

    start tests

--- src/lib.rs
+++ src/lib.rs
@@ -12,22 +12,35 @@ impl Config {
             return Err("not enough arguments");
         }
 
-        //program name is in args[0]
         let query = args[1].clone();
         let filename = args[2].clone();
-    
-        //return struct 
-        Ok(Config {
-            query,
-            filename
-        })
+
+        Ok(Config { query, filename })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
-    println!("File contents:\n\n{}", contents);
 
     Ok(())
 }
 
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    vec![]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
